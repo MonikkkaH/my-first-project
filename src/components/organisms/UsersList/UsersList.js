82f2e517b60889b63e1b6b0ea2c57e97f96ed83c
MonikkkaH <monika.hendzlik@gmail.com>
2021-06-05 00:14:03 +0200

    added hooks

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import { users } from 'data/users';
+import PropTypes from 'prop-types';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
-import { StyledList, Wrapper } from './UsersList.styles';
+import { StyledList } from './UsersList.styles';
+import { UserShape } from 'types';
+import { Title } from 'components/atoms/Title/Title';
 
-const UsersList = () => (
-  <Wrapper>
-    <StyledList>
-      {users.map((userData) => (
-        <UsersListItem key={userData.name} userData={userData} />
-      ))}
-    </StyledList>
-  </Wrapper>
-);
+const UsersList = ({ users }) => {
+  return (
+    <>
+      <Title>Students list</Title>
+      <StyledList>
+        {users.map((userData) => (
+          <UsersListItem key={userData.name} userData={userData} />
+        ))}
+      </StyledList>
+    </>
+  );
+};
+
+UsersList.propTypes = {
+  users: PropTypes.arrayOf(PropTypes.shape(UserShape)),
+  deleteUser: PropTypes.func,
+};
 
 export default UsersList;
 
